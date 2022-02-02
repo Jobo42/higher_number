82d0e8cadc50bcc8897a7a930875cbdcffd4135a
Jobo42 <liamhade@gmail.com>
2022-02-02 16:46:32 -0600

    Coded higher number game

--- higher_number.py
+++ higher_number.py
@@ -1,1 +1,19 @@
-"" 
+'''
+Game: user types in a number,
+and the computer tries to think of a 
+higher one.
+'''
+
+def higher_num(x):
+    return x + 1
+
+def get_user_number():
+    n = input("Pick any number:")
+    return float(n)
+
+def print_computer_number(n):
+    print(f"The computer played: {n}")
+
+user_number = get_user_number()
+computer_number = higher_num(user_number)
+print_computer_number(computer_number)
